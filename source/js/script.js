8e2712d68de4d46fa9688a76b2f19dbd3b37b37f
Julia Krotova <julia9867@mail.ru>
2019-12-01 03:05:48 +0700

    преимущества

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,24 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+document.addEventListener('DOMContentLoaded', function () {
+  var sectionsToggle = document.querySelector('.page-footer__sections-container button');
+  var addressToggle = document.querySelector('.page-footer__address-container button');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+  sectionsToggle.addEventListener('click', function () {
+    var parent = this.closest('.page-footer__sections');
+    if (parent.classList.contains('page-footer__sections--close')) {
+      parent.classList.remove('page-footer__sections--close');
+    } else {
+      parent.classList.add('page-footer__sections--close');
+    }
+  });
+
+  addressToggle.addEventListener('click', function () {
+    var parent = this.closest('.page-footer__address');
+    if (parent.classList.contains('page-footer__address--close')) {
+      parent.classList.remove('page-footer__address--close');
+    } else {
+      parent.classList.add('page-footer__address--close');
+    }
+  });
 });
