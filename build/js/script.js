27c739ca9846e9708018650a519257a5d01d165d
Julia Krotova <julia9867@mail.ru>
2019-12-02 12:57:54 +0700

    about us

--- build/js/script.js
+++ build/js/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
+  //event handlers -----------------------------
   var sectionsToggle = document.querySelector('.page-footer__sections-container button');
   var addressToggle = document.querySelector('.page-footer__address-container button');
 
-  sectionsToggle.addEventListener('click', function () {
+  var description = document.querySelector('.about-us__description');
+  var aboutUsToggle = document.querySelector('.about-us__button');
+
+  sectionsToggle.addEventListener('click', function() {
     var parent = this.closest('.page-footer__sections');
     if (parent.classList.contains('page-footer__sections--close')) {
       parent.classList.remove('page-footer__sections--close');
@@ -13,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  addressToggle.addEventListener('click', function () {
+  addressToggle.addEventListener('click', function() {
     var parent = this.closest('.page-footer__address');
     if (parent.classList.contains('page-footer__address--close')) {
       parent.classList.remove('page-footer__address--close');
@@ -21,4 +25,37 @@ document.addEventListener('DOMContentLoaded', function () {
       parent.classList.add('page-footer__address--close');
     }
   });
+
+  aboutUsToggle.addEventListener('click', function() {
+    description.classList.remove('about-us__description--height');
+    description.innerHTML = allHtml;
+  });
+
+  //clip text ----------------------------------
+  if (window.innerWidth < 1024) {
+    var clone = document.createElement('div');
+    clone.classList.add('about-us__description');
+
+    clone.style.position = 'absolute';
+    clone.style.visibility = 'hidden';
+    clone.style.width = description.clientWidth + 'px';
+    clone.innerHTML = description.innerHTML;
+    document.body.appendChild(clone);
+
+    var lastChild = clone.children[description.children.length - 1];
+    var text = lastChild.innerHTML;
+
+    var l = text.length - 1;
+    while (l >= 0 && clone.clientHeight > description.clientHeight) {
+
+      while (text[l] != ' ') l--;
+      lastChild.innerHTML = text.substring(0, l) + '...';
+      l--;
+
+    }
+
+    var allHtml = description.innerHTML;
+    description.innerHTML = clone.innerHTML;
+    clone.remove();
+  }
 });
